231c6948e6f1e40c0f01a06afbaca8824d21d0f4
jhonry-ninja <jcopinaldo@gmail.com>
2020-04-03 14:23:34 -0700

    Add files via upload

---
+++ web/src/components/home.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+    render() {
+        return (
+            <h3>Welcome home!</h3>
+
+        )
+
+    }
+
+}
